e9901de7ffa12de61620f1eb6f9d6e81b0768524
Shlok Sheth <ss3756@nau.edu>
2024-10-10 23:06:19 -0700

    Initial Commit

---
+++ StarupSound/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StarupSound
+//
+//  Created by Shlok Sheth on 10/10/24.
+//  Copyright © 2024 Shlok Sheth. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StarupSound/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StarupSound
+//
+//  Created by Shlok Sheth on 10/10/24.
+//  Copyright © 2024 Shlok Sheth. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
